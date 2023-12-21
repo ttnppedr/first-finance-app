5ecfe21e1a38c8f9e7342ff151dfad00993d3a9f
ttnppedr <ttnppedr@fastmail.com>
2023-12-21 15:27:14 +0800

    Add heading

--- lib/home.dart
+++ lib/home.dart
@@ -11,6 +11,57 @@ class Home extends StatelessWidget {
           children: [
             Container(
               width: double.infinity,
+              height: 240,
+              decoration: BoxDecoration(
+                color: Color(0xff368983),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                ),
+              ),
+              child: Stack(
+                children: [
+                  Positioned(
+                    top: 35,
+                    left: 340,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(7),
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        color: Color.fromRGBO(250, 250, 250, 0.1),
+                        child: Icon(
+                          Icons.notification_add_outlined,
+                          size: 30,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 35, left: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Good afternoon',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16,
+                              color: Color.fromARGB(255, 224, 223, 223)),
+                        ),
+                        Text(
+                          'Enjelin Morgeana',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 20,
+                              color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             )
           ],
         ),
