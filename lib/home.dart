2d1159999fd91ea682e770c70213f434f5ae0d32
ttnppedr <ttnppedr@fastmail.com>
2023-12-21 15:09:58 +0800

    Create home

---
+++ lib/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              width: double.infinity,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
